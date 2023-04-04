e7564ba062eeda357e8aa956c9d621e19c5d6e86
Beyza Sude <erolbeyzasude@gmail.com>
2023-04-04 12:55:37 +0300

    Initial Commit

---
+++ UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Beyza Sude Erol on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
