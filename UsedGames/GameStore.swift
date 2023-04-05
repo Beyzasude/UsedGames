5ff12dae0ad64e767bdb86fa9c9e5865a5b33b23
Beyza Sude <erolbeyzasude@gmail.com>
2023-04-05 14:46:55 +0300

    Second Commit

--- UsedGames/GameStore.swift
+++ UsedGames/GameStore.swift
@@ -7,21 +7,38 @@
 
 import Foundation
 
-class GameStore{
+class GameStore: ObservableObject {
     
-    var games: [Game] = []
+    @Published var games: [Game] = [] // ne zaman bir obje değiştiğinde arayüzün değişmesini istersek published çağırılır
     
-    init(){
-        
-        for _ in 0..<5{
-            createGames()
-        }
-    }
+    init() {}
     
     @discardableResult func createGames() -> Game{
-        
         let game = Game(random: true)
         games.append(game)
         return game
+    }
+    
+    func delete(at indexSet: IndexSet) {
+        games.remove(atOffsets: indexSet)
+    }
+    
+    func move(indices: IndexSet, to newOffset: Int) {
+        games.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func indexSet(for game: Game) -> IndexSet?{
+        if let firstIndex = games.firstIndex(of: game){
+            return IndexSet(integer: firstIndex)
+        }else{
+            return nil
+        }
+    }
+    
+    func game(at indexSet: IndexSet) -> Game? {
+        if let firstIndex = indexSet.first{
+            return games[firstIndex]
+        }
+        return nil
     }
 }
---
+++ UsedGames/Preview Content/Color+UsedGames.swift
@@ -0,0 +1,16 @@
+//
+//  Color+UsedGames.swift
+//  UsedGames
+//
+//  Created by Beyza Sude Erol on 5.04.2023.
+//
+
+import SwiftUI
+
+ //"extension" kelimesi, bir tipin veya yapısal bir nesnenin özelliklerinin veya işlevselliğinin genişletilmesine izin veren bir dil özelliğini ifade eder.
+extension Color {
+    
+    static var usedGamesWhite: Color {
+        return Color("UsedGamesWhite")
+    }
+}
